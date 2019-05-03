165531c4a72b892099644318d2a11104454ef4f5
Matthew Varnava <varnavamatthew@yahoo.co.uk>
2019-05-03 10:13:16 +0100

    Update RunPi.java

--- RunPi.java
+++ RunPi.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class RunPi{
-public static void main(String args[]){
-	System.out.println("How many decimals would you like to output pi to?");
-	Scanner reader = new Scanner(System.in);
-	Pi calcPi = new Pi();
-	int digs = reader.nextInt();
-	System.out.println("" + calcPi.pi(digs));
-}
+	public static void main(String args[]){
+		System.out.println("How many decimals would you like to output pi to?");
+		Scanner reader = new Scanner(System.in);
+		Pi calcPi = new Pi();
+		int digs = reader.nextInt();
+		System.out.println("" + calcPi.pi(digs));
+	}
 }
